f6e96b0aa4d210c01d58f50ef918fb5133f6b42b
IGDaniel <daniel.parr@outlook.com>
2016-08-18 23:31:38 +0100

    0.04
    -First path question added
    -time to load changed from 5 to 3
    -Gameover added
    -Added option to kill game at gameover

--- Rocked.bat
+++ Rocked.bat
@@ -13,7 +13,51 @@ echo                           A TEXT BASED GAME, PLEASE WAIT...
 ping localhost -n 5 >nul
 :Before
 cls
-echo Before the game begins please enter your desired name.
+echo Before the game begins please enter your desired name then click enter.
 echo. 
 set /p name=
 :startup
+cls
+echo. 
+echo You awake in a small gloomy room. Around you is a door on the left, a door on the right and deep dark hole right in front of you.
+
+echo (type the number of your choice then enter)
+
+echo.
+echo 1.)Check the left door.
+echo 2.)Check the right door.
+echo 3.)Jump down the deep dark hole.
+echo.
+set /p input0=Set Command:
+if %input0%==1 goto leftdoor1
+if %input0%==2 goto rightdoor1
+if %input0%==3 goto gameover
+goto startup
+:observe1
+
+:gameover
+cls
+echo        Oh no! Your dead! Dont fall for the same mistake twice! Try again!
+echo.
+echo.
+echo            000000 000000 0       0 000000    00000 0   0 000000 00000
+echo            0      0    0 00     00 0         0   0 0   0 0      0   0
+echo            0  000 000000 0 0   0 0 00000     0   0 0   0 00000  00000
+echo            0    0 0    0 0  0 0  0 0         0   0  0 0  0      0  0
+echo            000000 0    0 0   0   0 000000    00000   0   000000 0   0 
+echo.
+echo.
+
+echo.
+echo 1.)Start again!
+echo 2.)Close the game!
+echo.
+set /p input0=Set Command:
+if %input0%==1 goto startup
+if %input0%==2 goto killgame
+
+goto startup
+:observe2
+
+:killgame
+ping localhost -n 1 >nul
