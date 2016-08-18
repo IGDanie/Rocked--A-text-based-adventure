a1055aa9afc7571a603beb56db2d8de3090b1191
IGDaniel <daniel.parr@outlook.com>
2016-08-18 23:11:32 +0100

    0.02
    Home screen for the game.

--- Rocked.bat
+++ Rocked.bat
@@ -1,3 +1,14 @@
 @echo off
 title Rocked
 color 0c
+
+echo.
+echo                   000000 000000 000000 000000 0  000 000000 00000
+echo                   0    0 0    0 0    0 0      0 000  0      0    0
+echo                   000000 0    0 0    0 0      000    000000 0    0
+echo                   0 00   0    0 0    0 0      0 000  0      0    0
+echo                   0   00 000000 000000 000000 0  000 000000 00000
+echo.
+echo                           A TEXT BASED GAME, PLEASE WAIT...
+ping localhost -n 5 >nul
+:Before
