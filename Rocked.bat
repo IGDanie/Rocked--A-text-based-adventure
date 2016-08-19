d4b94019f1906678b04a0230b11643207139670b
IGDaniel <daniel.parr@outlook.com>
2016-08-19 01:12:31 +0100

    0.11
    -new ways to cause gameover
    -minor fixes once again
    -room 2 complete

--- Rocked.bat
+++ Rocked.bat
@@ -52,7 +52,7 @@ echo 3.)Give up and beat yourself to death with a rock that has presented itself
 set /p input0=Set Command:
 if %input0%==1 goto rightdoor1
 if %input0%==2 goto gameover
-if %input0%==3 goto gameover
+if %input0%==3 goto gameoverbeatself
 
 :rightdoor1
 cls
@@ -72,18 +72,86 @@ if %input0%==2 goto room2
 
 :room2
 cls
-echo  You enter through the door...
+echo You enter through the door...
 ping localhost -n 2 >nul 
-echo  Its cold... Theres snow beneath your feet...
+echo Its cold... Theres snow beneath your feet...
 ping localhost -n 2 >nul 
 echo What should you do now?
 ping localhost -n 2 >nul 
 echo.
-pause
+
+echo 1.)Make a snow angel... because why not...
+echo 2.)Figure out where you are.
+echo 3.)Explore.
+echo 4.)Go back through the door.
+echo.
+set /p input0=Set Command:
+if %input0%==1 goto gameoversnowangel
+if %input0%==2 goto figureout
+if %input0%==2 goto explore
+if %input0%==2 goto locked
+
+//gameover areas
+:gameoverbeatself
+cls
+echo Ah its as if by some miricale that a rock has appeared...
+ping localhost -n 2 >nul 
+echo You pick up the rock and start smashing it against your head non stop...
+ping localhost -n 2 >nul 
+echo Your dead... That was fun wasnt it...
+echo.
+echo.
+echo            000000 000000 0       0 000000    00000 0   0 000000 00000
+echo            0      0    0 00     00 0         0   0 0   0 0      0   0
+echo            0  000 000000 0 0   0 0 00000     0   0 0   0 00000  00000
+echo            0    0 0    0 0  0 0  0 0         0   0  0 0  0      0  0
+echo            000000 0    0 0   0   0 000000    00000   0   000000 0   0 
+echo.
+echo.
+
+echo.
+ping localhost -n 2 >nul 
+echo 1.)Start again!
+echo 2.)Close the game!
+echo.
+set /p input0=Set Command:
+if %input0%==1 goto before
+if %input0%==2 goto killgame
+
+:gameoversnowangel
+cls
+echo You lay down on the floor and spread your arms and legs ready to make the perfect snow angel...
+ping localhost -n 2 >nul 
+echo but before you know it out of no where comes a rock and splatters your brains...
+ping localhost -n 2 >nul 
+echo now that snow angel didnt sound like a good idea did it.
+ping localhost -n 2 >nul 
+echo.
+echo.
+echo            000000 000000 0       0 000000    00000 0   0 000000 00000
+echo            0      0    0 00     00 0         0   0 0   0 0      0   0
+echo            0  000 000000 0 0   0 0 00000     0   0 0   0 00000  00000
+echo            0    0 0    0 0  0 0  0 0         0   0  0 0  0      0  0
+echo            000000 0    0 0   0   0 000000    00000   0   000000 0   0 
+echo.
+echo.
+
+echo.
+ping localhost -n 2 >nul 
+echo 1.)Start again!
+echo 2.)Close the game!
+echo.
+set /p input0=Set Command:
+if %input0%==1 goto before
+if %input0%==2 goto killgame
 
 :gameover
 cls
-echo        Oh no! Your dead from rocks! Dont fall for the same mistake twice! Try again!
+echo You jump down the hole...
+ping localhost -n 2 >nul 
+echo Oh no! Your dead from falling down the deep hole full of rocks! 
+ping localhost -n 2 >nul 
+echo Dont fall for the same mistake twice! Try again!
 ping localhost -n 2 >nul 
 echo.
 echo.
